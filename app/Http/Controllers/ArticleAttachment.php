b8c5c7ca11eef68029742f7dc2230a2881c3804d
Jesko701 <IRZHunterBlood@yahoo.com>
2023-11-10 17:18:22 +0700

    fixing concurent on Group Article

--- app/Http/Controllers/ArticleAttachment.php
+++ app/Http/Controllers/ArticleAttachment.php
@@ -2,93 +2,131 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Article;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use App\Models\Article_attachment;
 use Illuminate\Http\Request;
+use Amp\Loop;
 
 class ArticleAttachment extends BaseController
 {
     public function all()
     {
-        $attachment = Article_attachment::all();
-        return response()->json([
-            'message' => 'berhasil mengambil seluruh data',
-            'data' => $attachment
-        ], 200);
+        try {
+            $attachment = null;
+
+            Loop::run(function () use (&$attachment) {
+                $attachmentPromise = \Amp\call(function () {
+                    return Article_attachment::all();
+                });
+                $attachment = yield $attachmentPromise;
+            });
+
+            return response()->json([
+                'message' => 'berhasil mengambil seluruh data',
+                'data' => $attachment
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Terjadi kesalahan saat mengambil data',
+                'error' => $th->getMessage(),
+            ], 500);
+        }
     }
 
     public function dataPagination(Request $request)
-{
-    $page = $request->input('page', 1);
-    $jumlah = (int)$request->input('jumlah', 50);
-    $offset = ($page - 1) * $jumlah;
+    {
+        $page = $request->input('page', 1);
+        $jumlah = (int)$request->input('jumlah', 50);
+        $offset = ($page - 1) * $jumlah;
 
-    try {
-        $data = Article_attachment::with('article')->skip($offset)->take($jumlah)->get();
-        return response()->json([
-            'message' => 'Data berhasil ditemukan',
-            'data' => $data
-        ], 200);
-    } catch (\Exception $error) {
-        return response()->json([
-            'message' => 'Terjadi kesalahan saat mengambil data',
-            'error' => $error->getMessage(),
-        ], 500);
+        try {
+            $data = null;
+            Loop::run(function () use ($jumlah, $offset, &$data) {
+                $promise = \Amp\call(function () use ($jumlah, $offset) {
+                    return Article_attachment::with('article')->skip($offset)->take($jumlah)->get();
+                });
+                $data = yield $promise;
+            });
+
+            return response()->json([
+                'message' => 'Data berhasil ditemukan',
+                'data' => $data
+            ], 200);
+        } catch (\Exception $error) {
+            return response()->json([
+                'message' => 'Terjadi kesalahan saat mengambil data',
+                'error' => $error->getMessage(),
+            ], 500);
+        }
     }
-}
 
 
     public function show($id)
     {
-        $attachment = Article_attachment::with('article')->find($id);
-        if (!$attachment) {
+        try {
+            $attachment = null;
+            Loop::run(function () use ($id, &$attachment){
+                $promiseAttachment = \Amp\call(function () use ($id){
+                    return Article_attachment::with('article')->find($id);
+                });
+                $attachment = yield $promiseAttachment;
+            });
+            if (!$attachment) {
+                return response()->json([
+                    'message' => 'data tidak ditemukan'
+                ], 404);
+            }
             return response()->json([
-                'message' => 'data tidak ditemukan'
-            ], 404);
+                'message' => 'data berhasil ditemukan',
+                'data' => $attachment
+            ], 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Terjadi kesalahan saat mengambil data',
+                'error' => $th->getMessage(),
+            ], 500);
         }
-        return response()->json([
-            'message' => 'data berhasil ditemukan',
-            'data' => $attachment
-        ],201);
     }
 
-    public function create(Request $request){
+    public function create(Request $request)
+    {
         $request['created_at'] = time();
-        
+
         $attachmentData = $request->all();
 
         $attachment = Article_attachment::create($attachmentData);
         return response()->json([
             'message' => 'data berhasil ditambahkan',
             'data' => $attachment
-        ],201);
+        ], 201);
     }
 
-    public function update(Request $request, $id){
+    public function update(Request $request, $id)
+    {
         $attachment = Article_attachment::find($id);
-        if (!$attachment){
+        if (!$attachment) {
             return response()->json([
                 'message' => 'data tidak ditemukan'
-            ],404);
+            ], 404);
         }
         $attachment->update($request->all());
         return response()->json([
             'message' => 'data berhasil diupdate',
             'data' => $attachment
-        ],200);
+        ], 200);
     }
 
-    public function hapus($id){
+    public function hapus($id)
+    {
         $attachment = Article_attachment::find($id);
-        if (!$attachment){
+        if (!$attachment) {
             return response()->json([
                 'message' => 'data tidak ditemukan'
-            ],404);
+            ], 404);
         }
         $attachment->delete();
         return response()->json([
             'message' => 'data berhasil dihapus'
-        ],200);
+        ], 200);
     }
 }
--- app/Http/Controllers/ArticleCategory.php
+++ app/Http/Controllers/ArticleCategory.php
@@ -5,96 +5,145 @@ namespace App\Http\Controllers;
 use App\Models\Article_category;
 use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Amp\Loop;
 
 class ArticleCategory extends BaseController
 {
-    public function all(){
-        $category = Article_category::all();
+    public function all()
+    {
+        $categoryData = [];
+
+        Loop::run(function () use (&$categoryData) {
+            $promises = [];
+            // Tambahkan setiap panggilan database sebagai promise
+            $promises['categories'] = \Amp\call(function () {
+                return Article_category::all()->toArray();
+            });
+            $results = yield $promises;
+            $categoryData = $results['categories'];
+        });
+
         return response()->json([
             'message' => 'berhasil mengambil seluruh data',
-            'data' => $category
-        ],200);
+            'data' => $categoryData
+        ], 200);
     }
 
     public function dataPagination(Request $request)
-{
-    try {
-        $page = $request->query('page', 1);
-        $jumlah = (int)$request->query('jumlah', 50);
-        $offset = ($page - 1) * $jumlah;
+    {
+        try {
+            $page = $request->query('page', 1);
+            $jumlah = (int)$request->query('jumlah', 50);
+            $offset = ($page - 1) * $jumlah;
 
-        $data = Article_category::with([
-            'article' => function ($query) use ($jumlah) {
-                $query->limit($jumlah);
-            }
-        ])->skip($offset)->take($jumlah)->get();
+            $data = null;
+            $totalData = null;
 
-        $totalData = Article_category::count();
+            Loop::run(function () use ($jumlah, $offset, &$data, &$totalData) {
+                $promise = [];
+                $promise['categories'] = \Amp\call(function () use ($jumlah, $offset) {
+                    return Article_category::with([
+                        'article' => function ($query) use ($jumlah) {
+                            $query->limit($jumlah);
+                        }
+                    ])->skip($offset)->take($jumlah)->get()->toArray();
+                });
+                $promise['totalData'] = \Amp\call(
+                    function () {
+                        return Article_category::count();
+                    }
+                );
 
-        return response()->json([
-            'message' => 'data berhasil ditemukan',
-            'data' => $data,
-            'total_data' => $totalData
-        ], 200);
-    } catch (\Exception $error) {
-        return response()->json([
-            'message' => 'Terjadi saat mengambil data',
-            'error' => $error->getMessage(),
-        ], 500);
-    }
-}
+                $result = yield $promise;
+                $data = $result['categories'];
+                $totalData = $result['totalData'];
+            });
 
-    public function show($id){
-        $category = Article_category::with('article')->find($id);
-        if (!$category) {
             return response()->json([
-                'message' => 'data tidak ditemukan'
-            ],404);
+                'message' => 'Data berhasil ditemukan',
+                'data' => $data,
+                'total_data' => $totalData,
+            ], 200);
+        } catch (\Exception $error) {
+            return response()->json([
+                'message' => 'Terjadi kesalahan saat mengambil data',
+                'error' => $error->getMessage(),
+            ], 500);
         }
-        return response()->json([[
-            'message' => 'data berhasil ditemukan',
-            'data' => $category
-        ]],200);
     }
 
-    public function create(Request $request){
+
+    public function show($id)
+    {
+        try {
+            $categoryData = null;
+
+            Loop::run(function () use ($id, &$categoryData) {
+                $categoryPromise = \Amp\call(function () use ($id) {
+                    return Article_category::with('article')->find($id);
+                });
+
+                $categoryData = yield $categoryPromise;
+            });
+
+            if (!$categoryData) {
+                return response()->json([
+                    'message' => 'Data tidak ditemukan'
+                ], 404);
+            }
+
+            return response()->json([
+                'message' => 'Data berhasil ditemukan',
+                'data' => $categoryData
+            ], 200);
+        } catch (\Exception $error) {
+            return response()->json([
+                'message' => 'Terjadi kesalahan saat mengambil data',
+                'error' => $error->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function create(Request $request)
+    {
         $request['created_at'] = time();
         $request['updated_at'] = time();
         $category = Article_category::create($request->all());
         return response()->json([
-            'message'=>'data berhasil ditambahkan',
+            'message' => 'data berhasil ditambahkan',
             'data' => $category
         ]);
     }
 
-    public function update(Request $request, $id){
+    public function update(Request $request, $id)
+    {
         $request['updated_at'] = time();
         $category = Article_category::find($id);
-        if (!$category){
+        if (!$category) {
             return response()->json([
                 'message' => 'data tidak ditemuakan'
             ]);
         }
         $category->update($request->all());
-        Article_category::where('parent_id',$id)->update(['parent_id'=>$category->id]);
+        Article_category::where('parent_id', $id)->update(['parent_id' => $category->id]);
         return response()->json([
             'message' => 'data berhasil diupdate',
             'data' => $category
-        ],200);
-    }   
+        ], 200);
+    }
 
-    public function hapus($id){
+    public function hapus($id)
+    {
         $category = Article_category::find($id);
-        if (!$category){
+        if (!$category) {
             return response()->json([
-                'message'=>"data tidak ditemukan",
+                'message' => "data tidak ditemukan",
             ]);
         }
-        Article_category::where('parent_id',$id)->delete();
+        Article_category::where('parent_id', $id)->delete();
         $category->delete();
         return response()->json([
             'message' => 'data berhasil dihapus'
-        ],200);
-
+        ], 200);
     }
 }
