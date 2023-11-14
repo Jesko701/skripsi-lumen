1b5a0a65de67a83b1d1ede0cd6e7ff1787cdcd3a
Jesko701 <IRZHunterBlood@yahoo.com>
2023-11-14 14:07:21 +0700

    async is not working either using reactphp and revolt

--- app/Http/Controllers/ArticleCategory.php
+++ app/Http/Controllers/ArticleCategory.php
@@ -3,25 +3,257 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article_category;
+use Illuminate\Auth\Access\Response as AccessResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Laravel\Lumen\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Log;
-use Revolt\EventLoop;
+use React\MySQL\Factory;
+use React\MySQL\QueryResult;
+use Illuminate\Http\Client\Pool;
+use Illuminate\Http\Client\Response;
+use React\EventLoop\Loop;
+use Exception;
+use React\Promise\Deferred;
+use React\Promise\Promise;
 
 class ArticleCategory extends BaseController
 {
+    protected $databaseHost;
+    protected $databaseName;
+    protected $databaseUser;
+    protected $databasePassword;
+    protected $connector;
+
+    public function __construct()
+    {
+        $this->databaseHost = env('DB_HOST');
+        $this->databaseName = env('DB_DATABASE');
+        $this->databaseUser = env('DB_USERNAME');
+        $this->databasePassword = env('DB_PASSWORD');
+        $factory = new Factory();
+        $this->connector = $factory->createLazyConnection($this->databaseUser . ':' . $this->databasePassword . '@' . $this->databaseHost . '/' . $this->databaseName);
+    }
+
+    //     // Return the promise
+    //     return $deferred->promise();
+    // }
+
+    // public function fetchData()
+    // {
+    //     $connection = $this->connector;
+
+    //     $deferred = new Deferred();
+
+    //     Loop::run(function () use ($connection, $deferred) {
+    //         $connection->query('SELECT * FROM article_category')
+    //             ->then(
+    //                 function (QueryResult $command) use ($deferred) {
+    //                     $dataFields = $command->resultFields;
+    //                     $dataRows = $command->resultRows;
+
+    //                     // Build an associative array from the result
+    //                     $formattedData = [];
+    //                     foreach ($dataRows as $row) {
+    //                         $formattedRow = [];
+    //                         foreach ($row as $key => $value) {
+    //                             // Use column name as key
+    //                             $formattedRow[$dataFields[$key]['name']] = $value;
+    //                         }
+    //                         $formattedData[] = $formattedRow;
+    //                     }
+
+    //                     // Resolve the deferred with the formatted data
+    //                     $deferred->resolve($formattedData);
+    //                 },
+    //                 function (Exception $err) use ($deferred) {
+    //                     // Reject the deferred with an error
+    //                     $deferred->reject($err);
+    //                 }
+    //             );
+    //     });
+
+    //     // Yield the promise to wait for it to resolve
+    //     yield $deferred->promise();
+    // }
     public function all()
     {
-        $data = null;
-        EventLoop::defer(function () use (&$data) {
-            $data = Article_category::all();
-        });
-        EventLoop::run();
-        return response()->json([
-            'message' => 'data berhasil diambil',
-            'data' => $data
-        ]);
+        $connection = $this->connector;
+        // * Menggunakan Pool
+
+        // ! coba pakai method newFetchData(gagal)
+        // return $this->newfetchData()->then(
+        //     function ($result) {
+        //         return response()->json([
+        //             'message' => 'data berhasil diambil',
+        //             'data' => $result
+        //         ]);
+        //     },
+        //     function ($error) {
+        //         return response()->json([
+        //             'message' => 'terjadi kesalahan dalam mengambil data di database',
+        //             'error' => $error
+        //         ], 500);
+        //     }
+        // );
+        // ! mencoba printing data (Gagal) 
+        // $result = yield $this->fetchData();
+        // return response()->json([
+        //     'message' => 'data berhasil diambil',
+        //     'data' => $result
+        // ]);
+        // ! Data tidak ke print
+        // Loop::run(function () use ($connection) {
+        //     $deferred = new Deferred();
+
+        //     $connection->query('SELECT * FROM article_category')
+        //         ->then(
+        //             function (QueryResult $command) use ($deferred) {
+        //                 $dataFields = $command->resultFields;
+        //                 $dataRows = $command->resultRows;
+
+        //                 // Build an associative array from the result
+        //                 $formattedData = [];
+        //                 foreach ($dataRows as $row) {
+        //                     $formattedRow = [];
+        //                     foreach ($row as $key => $value) {
+        //                         // Use column name as key
+        //                         $formattedRow[$dataFields[$key]['name']] = $value;
+        //                     }
+        //                     $formattedData[] = $formattedRow;
+        //                 }
+
+        //                 // Resolve the deferred with the formatted data
+        //                 $deferred->resolve($formattedData);
+        //             },
+        //             function (Exception $err) use ($deferred) {
+        //                 // Reject the deferred with an error
+        //                 $deferred->reject($err);
+        //             }
+        //         );
+
+        //     // Wait for the deferred to be resolved and get the result
+        //     $result = yield $deferred->promise();
+
+        //     // Return or output the result
+        //     echo json_encode($result, JSON_PRETTY_PRINT);
+        // });
+        // * testing (keluar hasil tetapi berupa array)
+        $deferred = new Deferred();
+        $resultPromise = $deferred->promise($connection->query("SELECT * FROM article_category")->then(
+            function (QueryResult $command) {
+                // print_r($command->resultRows);
+                // echo count($command->resultRows) . ' row(s) in set' . PHP_EOL;
+                return json_encode($command->resultRows, JSON_PRETTY_PRINT);
+            },
+            function (Exception $err) {
+                echo 'Error: ' . $err->getMessage() . PHP_EOL;
+            }
+        ));
+        // $deferred->resolve($resultPromise);
+                
+        // // * Promise Fail
+        // $query = "SELECT * FROM article_category";
+        // $hasilData = null;
+        // $this->ambilDataDariDatabase($query)
+        //     ->then(function ($hasil) use (&$hasilData) {
+        //         $hasilData = $hasil;
+        //     })
+        //     ->otherwise(function ($err) {
+        //         return response()->json([
+        //             'message' => 'terjadi kesalahan dalam mengambil data',
+        //             'error' => $err->getMessage()
+        //         ], 500);
+        //     });
+
+        // $connection->query("SELECT * FROM article_category")->then(
+        //     function (QueryResult $command) {
+        //         print_r($command->resultRows);
+        //         echo count($command->resultRows) . ' row(s) in set' . PHP_EOL;
+        //     },
+        //     function (Exception $err) {
+        //         echo 'Error: ' . $err->getMessage() . PHP_EOL;
+        //     }
+        // );
+
+        // ! Tanpa menggunakan loop run & perlu membuat fungsi handle (tidak muncul)
+        // $data = null;
+        // $dataFile = null;
+
+        // $deferred = new Deferred();
+
+        // $connection->query('SELECT * FROM article_category')
+        //     ->then(function (QueryResult $command) use (&$data, &$dataField, $deferred) {
+        //         $data = $command->resultRows;
+        //         $dataField = $command->resultFields;
+        //         $deferred->resolve($data);
+        //     }, function (\Throwable $reason) use ($deferred) {
+        //         $exception = new \Exception($reason->getMessage(), $reason->getCode(), $reason);
+        //         $deferred->reject($exception);
+        //     });
+
+        // $promise = $deferred->promise();
+        // return $this->handlePromiseGetAllCategory($promise);
+
+        // ! Menggunakan Loop Run (Data tidak muncul)
+        // Loop::run(function () use ($connection) {
+        //     $deferred = new Deferred();
+        //     $promise_data = $connection->query('SELECT * FROM article_category')
+        //         ->then(function (QueryResult $command) use (&$data, &$dataField, $deferred) {
+        //             $data = $command->resultRows;
+        //             $dataField = $command->resultFields;
+        //             Log::info('data di database sudah diambil secara async');
+        //             $deferred->resolve([$data, $dataField]);
+        //         }, function ($err) use ($deferred) {
+        //             return response()->json([
+        //                 'message' => 'terjadi kesalahan dalam mengambil data di database',
+        //                 'error' => $err
+        //             ], 500);
+        //         });
+
+        //     // Mendapatkan hasil promise data dan printing
+        //     $promise_data->then(function ($finalData) use ($deferred) {
+        //         $deferred->promise()->then(function () use ($finalData){
+        //             return response()->json([
+        //                 'message' => 'data berhasil diambil',
+        //                 'data' => $finalData
+        //             ]);
+        //         });
+        //     }, function ($err) {
+        //         return response()->json($err,500);
+        //     });
+        // });
+    }
+
+    // public function handlePromiseGetAllCategory(PromiseInterface $promise)
+    // {
+    //     Loop::run(function () use ($promise) {
+    //         $promise->then(
+    //             function ($finalData) {
+    //                 return response()->json([
+    //                     'message' => 'data berhasil diambil',
+    //                     'data' => $finalData
+    //                 ]);
+    //             },
+    //             function (Throwable $err) {
+    //                 return response()->json([
+    //                     'message' => 'terjadi kesalahan dalam mengambil data di database',
+    //                     'error' => $err->getMessage()
+    //                 ]);
+    //             }
+    //         );
+    //     });
+    // }
+
+    protected function ambilDataDariDatabase($query)
+    {
+        $connection = $this->connector;
+        return $connection->query($query)
+            ->then(function (QueryResult $command) {
+                $data = $command->resultRows;
+                return $data;
+            }, function ($err) {
+                throw $err;
+            });
     }
 
     public function dataPagination(Request $request)
