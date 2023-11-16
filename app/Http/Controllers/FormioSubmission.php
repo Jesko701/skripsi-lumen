480976239091facde4b8a264b30f99b08e4fe021
Jesko701 <IRZHunterBlood@yahoo.com>
2023-11-16 16:51:20 +0700

    fixing offset and limit on FormioSubmission method all()

--- app/Http/Controllers/FormioSubmission.php
+++ app/Http/Controllers/FormioSubmission.php
@@ -8,10 +8,13 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class FormioSubmission extends BaseController
 {
-    public function all()
+    public function all(Request $request)
     {
         try {
-            $data = Formio_submission::all();
+            $page = $request->input('page', 1);
+            $itemsPerPage = 75;
+            $offset = ($page - 1) * $itemsPerPage;
+            $data = Formio_submission::select('*')->offset($offset)->limit($itemsPerPage)->get();
             return response()->json([
                 'message' => 'data berhasil ditemukan',
                 'data' => $data,
